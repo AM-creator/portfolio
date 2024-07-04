8c96de0180efa78c3724fad7fd0d8c4560d2d99c
Angela <angelayuqi@gmail.com>
2024-07-03 23:21:40 -0400

    Update navbar toggler icon position

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -51,9 +51,9 @@ export const Banner = () => {
   return (
     <section className="banner" id="home">
       <Container>
-        <Row className="aligh-items-center">
+        <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <div>
+            <div class="intro">
               <span className="tagline">Welcome to my Portfolio</span>
               <h1>
                 {`Hi, I'm Angela`}{" "}
